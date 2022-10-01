a183488a57194d6e31e4e10b73d54fd06343d442
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-10-01 22:40:12 +0200

    Update script.js
    End of day 1

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ console.log("App version 1.0");
 let firstName = 'Pawel';
 console.log(firstName);
 console.log(firstName);
+
+///Variable should start with lower cases 
+///upper cases are reserved for object oriented programming
+/// name variables like this firstNamePerson etc.
+
+let PI = 3.1415;
+
+///variables are need to be descriptive 
+
+let myJob1 = 'Tester'; /// Those are more descriptive
+let myJob2 = 'Father'; ///
+
+let job1 = 'Tester';
+let job2 = 'Father';
+
+
