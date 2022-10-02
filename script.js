527b0e2007f7e24c2088a6f6989423970860cdea
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-10-02 22:16:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,32 @@ let firstName = 'Pawel';
 console.log(firstName);
 console.log(firstName);
 
+let PI = 3.1415;
+
 ///Variable should start with lower cases 
 ///upper cases are reserved for object oriented programming
 /// name variables like this firstNamePerson etc.
-
-let PI = 3.1415;
-
 ///variables are need to be descriptive 
-
-let myJob1 = 'Tester'; /// Those are more descriptive
-let myJob2 = 'Father'; ///
 
 let job1 = 'Tester';
 let job2 = 'Father';
+let myJob1 = 'Tester'; /// Those are more descriptive
+let myJob2 = 'Father'; ///
 
+
+///types of variables 
+let numbers = 12345;
+
+///Strings - text
+let letters = 'Lorem ipsum';
+
+/// Boolean - used for taking decisions
+let jsAmazing = true;
+
+/// undefined 
+let notSure;
+
+///Null - empty value
+///Symbol - not useful for now
+///BigInt - for bigger numbers 
 
