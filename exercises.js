68264d45016af3ee98f1b2f98e707d6f46457438
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-10-02 21:55:39 +0200

    New file and index update

---
+++ exercises.js
@@ -0,0 +1,7 @@
+let myCountry = 'Poland';
+let countryContinent = 'Europe';
+let myCountryPopulation = 38000000;
+
+console.log(myCountry);
+console.log(countryContinent);
+console.log(myCountryPopulation);
