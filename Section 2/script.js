c6e39ab92d11b821466ab8b0eda354aea79f3cc1
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-10-15 22:50:26 +0200

    Script update

---
+++ Section 2/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+//const interface = 'Audio';
+//const private = 534;
+
+//Functions
+
+function logger() {
+    console.log('My name is Paweł');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
