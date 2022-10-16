1d3bfcb5ab198c8ca8a7915eb85ec2c26ee97305
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-10-16 22:15:51 +0200

    Update script.js
    Functions lecture

--- Section 2/script.js
+++ Section 2/script.js
@@ -20,3 +20,20 @@ logger();
 logger();
 logger();
 
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(0, 5);
+console.log(orangeJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+
+
