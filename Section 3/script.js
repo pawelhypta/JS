2bdc2364ad86b781a8e2bd45e843b6aba4f117c3
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-03 22:29:56 +0100

    Update script.js

--- Section 3/script.js
+++ Section 3/script.js
@@ -1,7 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+const temperatures = [3, -2, -6, -1, 'error'9, 13, 17, 15, 14, 9, 5];
 
-const x = 1;
+const calcTempApltude = function (temps) {};
 
-console.log('Go live');
-console.log('12345');
+let max = temps[0];
+for (let i = 0; 1 <temps.lenght; i++ ){
+    if(temps[i] >)
+}
