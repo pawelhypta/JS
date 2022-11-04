61e5b79c90982f411736a57f6cf45e126b0e6e05
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-04 22:10:14 +0100

    Update script.js
    Could not find a fix.

--- Section 3/script.js
+++ Section 3/script.js
@@ -1,10 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-const temperatures = [3, -2, -6, -1, 'error'9, 13, 17, 15, 14, 9, 5];
+const temperatures = [3, 2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+console.log(temperatures);
 
-const calcTempApltude = function (temps) {};
+const calcTempApltude = function (temps) {
+  for (let i = 0; i < temps.lenght; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(max);
+};
 
-let max = temps[0];
-for (let i = 0; 1 <temps.lenght; i++ ){
-    if(temps[i] >)
-}
+calcTempApltude([3, 7, 4]);
