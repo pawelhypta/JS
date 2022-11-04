3fd27f0afdc6734d7046be46331120bc782a14ee
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-04 23:30:01 +0100

    Started section 4

--- Section 3/script.js
+++ Section 3/script.js
@@ -4,6 +4,7 @@ const temperatures = [3, 2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 console.log(temperatures);
 
 const calcTempApltude = function (temps) {
+  let max = temps[0];
   for (let i = 0; i < temps.lenght; i++) {
     if (temps[i] > max) max = temps[i];
   }
@@ -11,3 +12,24 @@ const calcTempApltude = function (temps) {
 };
 
 calcTempApltude([3, 7, 4]);
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celcius',
+
+    //(C)Fix the bug
+    value: Number(prompt('Degrees celcius:')),
+  };
+  //(B) Find the bug
+  console.log(measurement);
+  console.table(measurement);
+  console.log(measurement.value);
+  //console.warn(measurement.value);
+  //console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+//(A) Identify the bug
+console.log(measureKelvin());
