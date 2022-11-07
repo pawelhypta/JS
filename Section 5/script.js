f7f375d9bc1c654e8dc28be90378cd290c32d96b
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-07 22:20:27 +0100

    Update script.js

--- Section 5/script.js
+++ Section 5/script.js
@@ -1,1 +1,12 @@
 'use strict';
+//DOM and DOM manipulation
+//Documented object model - structured representation of the HTML
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🏆 Correct number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 13;
+
+document.querySelector('');
