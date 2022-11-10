0801d17167b9106166d61c26f1592d6c6f7a6a4b
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-10 23:59:01 +0100

    Update script.js

--- Section 5/script.js
+++ Section 5/script.js
@@ -11,12 +11,19 @@ document.querySelector('.score').textContent = 13;
 
 document.querySelector('.guess').value = 23;*/
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if (guess > 20 || guess < 1) {
-    document.querySelector('.message').textContent =
-      'Please enter a valid number! ⛔️';
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number! ⛔️';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🏆 Correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!📈';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!📉';
   }
 });
