212f137faed86263115d2430d95c11068e432cbe
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-10 23:33:55 +0100

    Update script.js

--- Section 5/script.js
+++ Section 5/script.js
@@ -2,11 +2,21 @@
 //DOM and DOM manipulation
 //Documented object model - structured representation of the HTML
 
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🏆 Correct number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 13;
 
-document.querySelector('');
+document.querySelector('.guess').value = 23;*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (guess > 20 || guess < 1) {
+    document.querySelector('.message').textContent =
+      'Please enter a valid number! ⛔️';
+  }
+});
