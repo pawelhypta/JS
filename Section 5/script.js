bf1177992fd7349d8334e2e432affb0a9cf64166
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-11 22:06:42 +0100

    Update script.js

--- Section 5/script.js
+++ Section 5/script.js
@@ -12,6 +12,7 @@ document.querySelector('.score').textContent = 13;
 document.querySelector('.guess').value = 23;*/
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,8 +23,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🏆 Correct number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!📈';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over! 💀';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low!📉';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over! 💀';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
