691458c084941c9e554b2db756b7c1efd281bb0b
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-18 22:48:43 +0100

    Update script.js

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -2,8 +2,32 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+let currentScore = 0;
+let activePlayer = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //2. Display correct dice photo
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //3. Check for rolled 1. If one is rolled we need to switch the player
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+    //TO BE changed later
+  } else {
+    //switch player
+  }
+});
