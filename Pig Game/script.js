e4bb09abc11658c5ecd5aed976c4ee6fdeb8a231
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-19 22:48:34 +0100

    Update script.js

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -8,9 +9,18 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 let currentScore = 0;
 let activePlayer = 0;
+const scores = [0, 0];
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -25,9 +35,26 @@ btnRoll.addEventListener('click', function () {
   //3. Check for rolled 1. If one is rolled we need to switch the player
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     //TO BE changed later
   } else {
     //switch player
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player-winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player-active');
+  }
+  switchPlayer();
 });
