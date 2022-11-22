c6b4950725f41f8d6ca1345e395622a16e9666b6
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-22 22:54:05 +0100

    Script update

--- Section 8/script.js
+++ Section 8/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  function printAge{
+    const output
+  }
+  return age;
+}
+
+const firstName = 'Pawel';
+calcAge(1991);
