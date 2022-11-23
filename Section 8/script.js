83a4596a017fe0aa35d2456b46b9d07765878a31
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-11-23 21:47:45 +0100

    Update script.js
    Practicing scoping

--- Section 8/script.js
+++ Section 8/script.js
@@ -2,9 +2,27 @@
 
 function calcAge(birthYear) {
   const age = 2022 - birthYear;
-  function printAge{
-    const output
+
+  function printAge() {
+    let output = `${firstName} you are born in ${birthYear} so you are ${age} years old.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const firstName = 'Not Pawel';
+      var millenial = true;
+      const str = `Oh, ${firstName} is a millenial`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      output = 'NEW OUTPUT';
+    }
+
+    console.log(millenial);
+    console.log(output);
   }
+  printAge();
   return age;
 }
 
