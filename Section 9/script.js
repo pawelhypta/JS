14ab164a02d6f95f1697fc8b4d7e714d4f4ba515
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2022-12-13 22:03:29 +0100

    Update script.js

--- Section 9/script.js
+++ Section 9/script.js
@@ -79,6 +79,14 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
+
+//Spread operator
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
 /*
 //Array destrctioning = method of breaking down/unpacking arrays into variables
 
